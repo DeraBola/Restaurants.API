e54720244fe442422ce7898fc3a3475a0fbcb57d
derabola <adu.deraj@gmail.com>
2024-11-03 12:57:18 -0800

    added ost for example weatherforecast

--- Restaurants.API/Controllers/WeatherForecastController.cs
+++ Restaurants.API/Controllers/WeatherForecastController.cs
@@ -2,6 +2,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Restaurants.API.Controllers
 {
+
+	public class TemperatureRequest
+	{
+		public int Min { get; set; }
+		public int Max { get; set; }
+	}
+
 	[ApiController]
 	[Route("api/[controller]")]
 	public class WeatherForecastController : ControllerBase
@@ -16,11 +23,24 @@ namespace Restaurants.API.Controllers
 			_weatherForcastService = weatherForcastService;
 		}
 
-		[HttpGet(Name = "GetWeatherForecast")]
+		[HttpPost("Generate")]
+		public IActionResult Generate([FromQuery] int count, [FromBody] TemperatureRequest request)
+		{
+			if (count < 0 || request.Max < request.Min)
+			{
+				return BadRequest("Count must be positive number, max value should be greater than min value");
+			}
+
+			var result = _weatherForcastService.Get(count, request.Min, request.Max);
+			return Ok(result);
+		}
+
+	/*	[HttpGet(Name = "GetWeatherForecast")]
 		public IEnumerable<WeatherForecast> Get()
 		{
 			var result = _weatherForcastService.Get();
 			return result;
 		}
+	*/
 	}
 }
