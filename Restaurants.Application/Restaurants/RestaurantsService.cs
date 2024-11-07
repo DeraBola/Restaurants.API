5f767ce51b84141c5b46d7b58a88f24c6793908b
derabola <adu.deraj@gmail.com>
2024-11-07 08:38:40 -0800

    added restaurant DTO

--- Restaurants.Application/Restaurants/RestaurantsService.cs
+++ Restaurants.Application/Restaurants/RestaurantsService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 
@@ -6,14 +7,17 @@ namespace Restaurants.Application.Restaurants
 {
 	internal class RestaurantsService(IRestaurantsRepository restaurantsRepository, ILogger<RestaurantsService> logger) : IRestaurantsService
 	{
-		public async Task<IEnumerable<Restaurant>> GetAllRestaurants()
+		public async Task<IEnumerable<RestaurantDto>> GetAllRestaurants()
 		{
 			logger.LogInformation("Getting all Restaurants");
 			var restaurants = await restaurantsRepository.GetAllAsync();
-			return restaurants;
+
+			var restaurantsDto = restaurants.Select(RestaurantDto.FromEntity);
+		
+			return restaurantsDto;
 		}
 
-		public async Task<Restaurant?> GetRestaurantById(int id)
+		public async Task<RestaurantDto?> GetRestaurantById(int id)
 		{
 			logger.LogInformation($"Getting all Restaurants {id}");
 			var restaurants = await restaurantsRepository.GetByIdAsync(id);
