e4176b7c7a3be8895e48c811f8167bce2c405dc8
derabola <adu.deraj@gmail.com>
2025-02-26 14:12:17 -0800

    userContext test

--- Restaurants.ApplicationTests1/Users/UserContextTests.cs
+++ Restaurants.ApplicationTests1/Users/UserContextTests.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Restaurants.Domain.Constants;
+using System.Security.Claims;
 
-namespace Restaurants.ApplicationTests1.Users
+namespace Restaurants.Application.Users.Tests
 {
-	internal class UserContextTests
+	public class UserContextTests
 	{
+		[Fact()]
+		public void GetCurrentUser_WithAuthenticatedUser_ShouldReturnCurrentUser()
+		{
+			// arrange
+			var dateOfBirth = new DateOnly(1990, 1, 1);
+
+			var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+			var claims = new List<Claim>()
+			{
+				new(ClaimTypes.NameIdentifier, "1"),
+				new(ClaimTypes.Email, "test@test.com"),
+				new(ClaimTypes.Role, UserRoles.Admin),
+				new(ClaimTypes.Role, UserRoles.User),
+				new("Nationality", "German"),
+				new("DateOfBirth", dateOfBirth.ToString("yyyy-MM-dd"))
+			};
+
+			var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Test"));
+
+			httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+			{
+				User = user
+			});
+
+			var userContext = new UserContext(httpContextAccessorMock.Object);
+
+			// act
+			var currentUser = userContext.GetCurrentUser();
+			Console.WriteLine(string.Join(", ", currentUser.Roles));
+
+
+			// asset
+
+			currentUser.Should().NotBeNull();
+			currentUser.Id.Should().Be("1");
+			currentUser.Email.Should().Be("test@test.com");
+			currentUser.Roles.Should().ContainInOrder(UserRoles.Admin, UserRoles.User);
+			// currentUser.Roles.Should().BeEquivalentTo(new[] { UserRoles.Admin, UserRoles.User });
+			currentUser.Nationality.Should().Be("German");
+			currentUser.DateOfBirth.Should().Be(dateOfBirth);
+
+		}
 	}
 }
--- Restaurants.Domain/Constants/UserRoles.cs
+++ Restaurants.Domain/Constants/UserRoles.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Restaurants.Domain.Constants
 {
 	public static class UserRoles
 	{
+		public const string Admin = "Admin";
 		public const string User = "User";
 		public const string Owner = "Owner";
-		public const string Admin = "Admin";
 	}
 }
