75910681dfae4b74abff983e466433821efc81c8
derabola <adu.deraj@gmail.com>
2025-02-26 18:52:16 -0800

    added test for validator

--- Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantDtoValidator.cs
+++ Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantDtoValidator.cs
@@ -5,19 +5,21 @@ namespace Restaurants.Application.Restaurants.Commands.CreateRestaurant
 {
     public class CreateRestaurantCommandValidator : AbstractValidator<CreateRestaurantCommand>
     {
-        private readonly List<string> validCategories = ["Italian", "Nigerian", "Mexcian", "American"];
+        private readonly List<string> validCategories = ["Italian", "Nigerian", "Mexican", "American"];
 
         public CreateRestaurantCommandValidator()
         {
             RuleFor(dto => dto.Name).Length(3, 100);
             RuleFor(dto => dto.Description).NotEmpty().WithMessage("Description is required");
             RuleFor(dto => dto.Category)
-                .Must(category => validCategories.Contains(category))
-                .WithMessage("Invalid categories. Please choose from the valid categories");
-            //RuleFor(dto => dto.Category).NotEmpty().WithMessage("Insert a valid category");
-            RuleFor(dto => dto.ContactEmail).EmailAddress().WithMessage("Please provide a valid email address");
-            RuleFor(dto => dto.PostalCode).Matches(@"^\d{2}\d{3}$").WithMessage("Please provide a valid postal code (xx-xxx)");
-            RuleFor(dto => dto.ContactNumber).Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Please provide a valid phone number");
+			//.Must(category => validCategories.Contains(category)) // lambda version
+			.Must(validCategories.Contains) // or directly feed the incoming argument
+			.WithMessage("Invalid category. Plese choose from the valid categories");
+			RuleFor(dto => dto.ContactEmail).EmailAddress().WithMessage("Please provide a valid email address");
+			RuleFor(dto => dto.PostalCode)
+			.Matches(@"^\d{2}-\d{3}$")
+			.WithMessage("Please provide a valid postal code in the format XX-XXX");
+			RuleFor(dto => dto.ContactNumber).Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Please provide a valid phone number");
         }
     }
 }
--- Restaurants.Application/Restaurants/Dtos/RestaurantsProfile.cs
+++ Restaurants.Application/Restaurants/Dtos/RestaurantsProfile.cs
@@ -9,21 +9,40 @@ namespace Restaurants.Application.Restaurants.Dtos
 	{
 		public RestaurantsProfile() {
 
-			CreateMap<UpdateRestaurantCommand, Restaurant>();
+			// CreateMap<UpdateRestaurantCommand, Restaurant>()
 
-			CreateMap<CreateRestaurantCommand, Restaurant>()
-				.ForMember(d => d.Address, opt => opt.MapFrom(
+			CreateMap<Restaurant, RestaurantDto>() // Create mapping from src: Restaurant to dest RestaurantDto.
+		  .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.Address == null ? null : src.Address.City))
+		  .ForMember(dest => dest.PostalCode, opt => opt.MapFrom(src => src.Address == null ? null : src.Address.PostalCode))
+		  .ForMember(dest => dest.Street, opt => opt.MapFrom(src => src.Address == null ? null : src.Address.Street))
+		  .ForMember(dest => dest.Dishes, opt => opt.MapFrom(src => src.Dishes)); // We also need to create a mapping from Dish to DishDto
+
+
+			CreateMap<CreateRestaurantCommand, Restaurant>() // Create mapping from src: CreateRestaurantDto to dest Restaurant.
+				.ForMember(dest => dest.Address, opt => opt.MapFrom(
 					src => new Address
 					{
 						City = src.City,
 						PostalCode = src.PostalCode,
 						Street = src.Street,
 					}));
+			CreateMap<UpdateRestaurantCommand, Restaurant>();
 
-			CreateMap<Restaurant, RestaurantDto>()
-				.ForMember(d => d.Address, opt => opt.MapFrom(src => src.Address))
-				.ForMember(d => d.Dishes, opt =>
-		        opt.MapFrom(src => src.Dishes));
+
+			/*	CreateMap<CreateRestaurantCommand, Restaurant>()
+					.ForMember(d => d.Address, opt => opt.MapFrom(
+						src => new Address
+						{
+							City = src.City,
+							PostalCode = src.PostalCode,
+							Street = src.Street,
+						}));
+
+				CreateMap<Restaurant, RestaurantDto>()
+					.ForMember(d => d.Address, opt => opt.MapFrom(src => src.Address))
+					.ForMember(d => d.Dishes, opt =>
+					opt.MapFrom(src => src.Dishes));
+			*/
 		}
 	}
 }
