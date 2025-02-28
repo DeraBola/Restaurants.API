0cb7f5a48c1e6778c80fa16e89f148419dc17e91
derabola <adu.deraj@gmail.com>
2025-02-28 12:40:45 -0800

    authorization policies test

--- Restaurants.Infrastructure/Authorization/Requirements/CreatedMultipleRestaurantsRequirement/CreatedMultipleRestaurantRequirementHandler.cs
+++ Restaurants.Infrastructure/Authorization/Requirements/CreatedMultipleRestaurantsRequirement/CreatedMultipleRestaurantRequirementHandler.cs
@@ -4,7 +4,7 @@ using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Infrastructure.Authorization.Requirements.CreatedMultipleRestaurantsRequirement;
 
-internal class CreatedMultipleRestaurantsRequirementHandler(IRestaurantsRepository restaurantsRepository,
+public class CreatedMultipleRestaurantsRequirementHandler(IRestaurantsRepository restaurantsRepository,
 	IUserContext userContext) : AuthorizationHandler<CreatedMultipleRestaurantsRequirement>
 {
 	protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context,
---
+++ Restaurants.InfrastructureTests/Authorization/Requirements/CreatedMultipleRestaurantsRequirement/CreatedMultipleRestaurantsRequirementHandlerTests.cs
@@ -0,0 +1,18 @@
+﻿using Restaurants.Infrastructure.Authorization.Requirements.CreatedMultipleRestaurantsRequirement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Infrastructure.Authorization.Requirements.CreatedMultipleRestaurantsRequirement.Tests
+{
+	public class CreatedMultipleRestaurantsRequirementHandlerTests
+	{
+		[Fact()]
+		public void HandleRequirementAsyncTest()
+		{
+
+		}
+	}
+}
